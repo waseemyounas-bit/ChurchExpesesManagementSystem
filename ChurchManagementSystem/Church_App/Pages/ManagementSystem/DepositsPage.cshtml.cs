0d4de17e6102588dee7481fa68ac826aaf918704
Muhammad Tanzeel <mt7034133@gmail.com>
2025-03-21 11:53:22 +0500

    Bank Deposits & Banks Template Done

---
+++ ChurchManagementSystem/Church_App/Pages/ManagementSystem/DepositsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Church_App.Pages.ManagementSystem
+{
+    public class DepositsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
