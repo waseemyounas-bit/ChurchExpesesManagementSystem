7ad5f921de6d9b50b142184e0631a6dd6ea2a6ad
Muhammad Tanzeel <mt7034133@gmail.com>
2025-03-21 12:22:26 +0500

    Groups Page  Template Done

---
+++ ChurchManagementSystem/Church_App/Pages/ManagementSystem/GroupsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Church_App.Pages.ManagementSystem
+{
+    public class GroupsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
