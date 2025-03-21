d74451893794e4b8117b8ef09cceae32e49cef83
Muhammad Tanzeel <mt7034133@gmail.com>
2025-03-21 07:06:37 +0500

    Members Page template

---
+++ ChurchManagementSystem/Church_App/Pages/ManagementSystem/Memberspage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Church_App.Pages.ManagementSystem
+{
+    public class MemberspageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
