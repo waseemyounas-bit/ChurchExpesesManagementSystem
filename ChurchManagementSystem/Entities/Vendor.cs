0875d9d3a4244f4bdf649c86602a8f856074d870
Muhammad Tanzeel <mt7034133@gmail.com>
2025-03-25 06:11:31 +0500

    Update Vendor.cs

--- ChurchManagementSystem/Entities/Vendor.cs
+++ ChurchManagementSystem/Entities/Vendor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,37 @@ namespace Entities
     public class Vendor
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Vendor name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
         public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address cannot exceed 200 characters")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Account number is required")]
+        [StringLength(50, ErrorMessage = "Account number cannot exceed 50 characters")]
         public string AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City name cannot exceed 50 characters")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(50, ErrorMessage = "State name cannot exceed 50 characters")]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Zip code is required")]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid ZIP code")]
         public string Zip { get; set; }
     }
 }
